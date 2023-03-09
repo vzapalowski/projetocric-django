ffd4fde47e71c1996c848dbe33d00f4545b5b557
JeremiasPiontkoski <jeremiaspion@gmail.com>
2023-03-09 15:30:43 -0300

    Initial commit

---
+++ cities/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def arroioDosRatos(resquest):
+    return render(resquest, 'cities/arroio-dos-ratos.html')
---
+++ templates/static/js/routes/marker.js
@@ -0,0 +1,13 @@
+
+
+const markerIcons = {
+    food: "./../assets/iconsMarkers/restaurant.svg",
+    gas_station: "./../assets/iconsMarkers/gas_station.svg",
+    repair: "./../assets/iconsMarkers/repair.svg",
+    hotel: "./../assets/iconsMarkers/hotel.svg",
+    hospital: "./../assets/iconsMarkers/hospital.svg",
+    tourist_spots: "./../assets/iconsMarkers/tourist_spots.svg",
+    phamacy: "./../assets/iconsMarkers/pharmacy.svg",
+    shopping: "./../assets/iconsMarkers/shopping.svg",
+    drink: "./../assets/iconsMarkers/drink.svg"
+}

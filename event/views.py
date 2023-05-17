df69480e0f9db53801eab6cf02e86527d083e083
regisbrasil <regisregis334@gmail.com>
2023-05-17 12:23:14 -0300

    form and stuff

--- event/views.py
+++ event/views.py
@@ -9,23 +9,25 @@ from event.models.enrollment import Bond
 from event.models.how_knew import HowKnew
 from event.models.route_path import RoutePath
 
-
-
 class EventView(DetailView):
     model = Event
     template_name = 'events/index.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['event'] = Event.objects.get(pk=self.kwargs['pk'])
-        context['form'] = EnrollmentForm
-        context['bond'] = Bond.objects.all()
-        context['howKnew'] = HowKnew.objects.all()
-        context['routePath'] = RoutePath.objects.all()
+        event = Event.objects.get(pk=self.kwargs['pk'])
+        
+        context['event'] = event
+        context['form'] = lambda: EnrollmentForm(self.request.POST) if self.request.method == 'POST' else EnrollmentForm()
+        context['bond'] = lambda: Bond.objects.all()
+        context['howKnew'] = lambda: HowKnew.objects.all()
+        context['routePath'] = lambda: RoutePath.objects.all()
+        
         return context
 
 def enrollment(request, event_id):
     event = Event.objects.get(pk=event_id)
+    form = None
     if request.method == 'POST':
         form = EnrollmentForm(request.POST)
         if form.is_valid():
@@ -33,6 +35,13 @@ def enrollment(request, event_id):
             return redirect('events:event', pk=event.pk)
     else:
         form = EnrollmentForm()
-    context = {'event': event, 'form': form}
+    
+    context = {
+        'event': event,
+        'form': form,
+        'bond': Bond.objects.all(),
+        'howKnew': HowKnew.objects.all(),
+        'routePath': RoutePath.objects.all(),
+    }
+    
     return render(request, 'events/index.html', context)
-

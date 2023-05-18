09e41c87464df222553caee87acf75b93b5a8047
Jeremias Piontkoski <jeremiaspion@gmail.com>
2023-05-18 11:01:59 -0300

    Add dynamic anchors and content in home

--- home/views/home.py
+++ home/views/home.py
@@ -2,6 +2,8 @@ from django.views.generic.list import ListView
 
 from home.models import CityManager
 from cities.models import City
+from cities.models import Category
+from event.models import Event
 
 class PostHome(ListView):
     template_name = 'home/index.html'
@@ -13,4 +15,6 @@ class PostHome(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['homes'] = CityManager.objects.all()
+        context['categories_points'] = Category.objects.all()
+        context['events'] = Event.objects.all()
         return context
--- templates/static/js/home/data.js
+++ templates/static/js/home/data.js
@@ -5,7 +5,6 @@ fetch(Urls.home_cities)
     .then(res => res.json())
     .then(data => {
         let map = new Map();
-        console.log(data[0])
         map.setMap(data[0]['coordinates'].lat, data[0]['coordinates'].lng);
         map.addRoutes(data[0].routes);
         map.addPoints(data[0].points);

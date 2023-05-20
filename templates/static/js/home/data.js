3a711a1efe918ac16749649b634620c34f5f8800
Jeremias Piontkoski <jeremiaspion@gmail.com>
2023-05-20 00:02:40 -0300

    Fixing bugs

--- templates/static/js/home/data.js
+++ templates/static/js/home/data.js
@@ -1,11 +1,13 @@
 import { Map } from "../map/map.js";
 import { Urls } from "../helpers/urls.js";
 
-fetch(Urls.home_cities)
-    .then(res => res.json())
-    .then(data => {
-        let map = new Map();
-        map.setMap(data[0]['coordinates'].lat, data[0]['coordinates'].lng);
-        map.addRoutes(data[0].routes);
-        map.addPoints(data[0].points);
-})
+console.log(Urls.home_cities)
+
+// fetch(Urls.home_cities)
+//     .then(res => res.json())
+//     .then(data => {
+//         let map = new Map();
+//         map.setMap(data[0]['coordinates'].lat, data[0]['coordinates'].lng);
+//         map.addRoutes(data[0].routes);
+//         map.addPoints(data[0].points);
+// })

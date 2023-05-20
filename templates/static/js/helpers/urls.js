58cc851af9dea616686234f2ef7d88f5e1cb4702
Jeremias Piontkoski <101434008+JeremiasPiontkoski@users.noreply.github.com>
2023-05-19 23:29:01 -0300

    Update urls.js

--- templates/static/js/helpers/urls.js
+++ templates/static/js/helpers/urls.js
@@ -1,5 +1,5 @@
 export const Urls = {
-    cities: 'http://127.0.0.1:8000/api/cities/',
-    home_cities: 'http://127.0.0.1:8000/api/home_cities/',
-    events: 'http://127.0.0.1:8000/api/events/',
+    cities: 'http://127.0.0.1:3306/api/cities/',
+    home_cities: 'http://127.0.0.1:3306/api/home_cities/',
+    events: 'http://127.0.0.1:3306/api/events/',
 }

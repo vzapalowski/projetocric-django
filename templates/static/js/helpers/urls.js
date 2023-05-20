fb5964467f0564cebf4a68271367a22f87a7bfc6
Vanius Zapalowski <vanius.zapalowski@gmail.com>
2023-05-20 20:23:30 +0000

    fixing url helpers

--- templates/static/js/helpers/urls.js
+++ templates/static/js/helpers/urls.js
@@ -1,5 +1,5 @@
 export const Urls = {
-    cities: 'http://200.132.47.33/api/cities/',
-    home_cities: 'http://200.132.47.33/api/home_cities/',
-    events: 'http://200.132.47.33/api/events/',
+    cities: 'http://rota-cric.charqueadas.ifsul.edu.br/api/cities/',
+    home_cities: 'http://rota-cric.charqueadas.ifsul.edu.br/api/home_cities/',
+    events: 'http://rota-cric.charqueadas.ifsul.edu.br/api/events/'
 }

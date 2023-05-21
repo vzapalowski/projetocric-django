9093a91d6fd86518ecaf55d1587004d55a9f674e
Jeremias Piontkoski <jeremiaspion@gmail.com>
2023-05-21 14:39:33 -0300

    Change field name in route_path

--- event/models/route_path.py
+++ event/models/route_path.py
@@ -3,7 +3,7 @@ from django.db import models
 from cities.models import Route
 
 class RoutePath(models.Model):
-    name = models.CharField(max_length=30, verbose_name='Nome do caminho')
+    name = models.CharField(max_length=100, verbose_name='Nome do caminho')
     route = models.ForeignKey(Route, on_delete=models.CASCADE)
     time = models.CharField(max_length=30, verbose_name='Horário de Sáida')
     departure_location = models.CharField(max_length=50, verbose_name='Local de Partida')

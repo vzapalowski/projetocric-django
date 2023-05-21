f380d0889f6d729b478d6daf3719c027e545dc96
Jeremias Piontkoski <jeremiaspion@gmail.com>
2023-05-21 13:40:07 -0300

    Change field time in route_path

--- templates/static/js/helpers/urls.js
+++ templates/static/js/helpers/urls.js
@@ -1,5 +1,7 @@
 export const Urls = {
-    cities: 'http://rota-cric.charqueadas.ifsul.edu.br/api/cities/',
-    home_cities: 'http://rota-cric.charqueadas.ifsul.edu.br/api/home_cities/',
-    events: 'http://rota-cric.charqueadas.ifsul.edu.br/api/events/'
+    // cities: 'http://rota-cric.charqueadas.ifsul.edu.br/api/cities/',
+    // home_cities: 'http://rota-cric.charqueadas.ifsul.edu.br/api/home_cities/',
+    // events: 'http://rota-cric.charqueadas.ifsul.edu.br/api/events/'
+
+    
 }

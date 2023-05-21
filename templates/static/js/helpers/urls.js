50188d7ccb4870c6b445adbefff9a4ad790431ab
Jeremias Piontkoski <jeremiaspion@gmail.com>
2023-05-21 15:11:30 -0300

    Change buttons front event

--- templates/static/js/helpers/urls.js
+++ templates/static/js/helpers/urls.js
@@ -1,9 +1,5 @@
 export const Urls = {
-    // cities: 'http://rota-cric.charqueadas.ifsul.edu.br/api/cities/',
-    // home_cities: 'http://rota-cric.charqueadas.ifsul.edu.br/api/home_cities/',
-    // events: 'http://rota-cric.charqueadas.ifsul.edu.br/api/events/'
-
-    cities: 'http://127.0.0.1:8000/api/cities/',
-    home_cities: 'http://127.0.0.1:8000/api/home_cities/',
-    events: 'http://127.0.0.1:8000/api/events/'
+    cities: 'http://rota-cric.charqueadas.ifsul.edu.br/api/cities/',
+    home_cities: 'http://rota-cric.charqueadas.ifsul.edu.br/api/home_cities/',
+    events: 'http://rota-cric.charqueadas.ifsul.edu.br/api/events/'
 }

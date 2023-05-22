24f0ba4a081f64bd2189965c772804321d99e2b5
Jeremias Piontkoski <jeremiaspion@gmail.com>
2023-05-22 11:35:51 -0300

    Add field concentration in route_path

--- event/models/route_path.py
+++ event/models/route_path.py
@@ -5,8 +5,8 @@ from cities.models import Route
 class RoutePath(models.Model):
     name = models.CharField(max_length=100, verbose_name='Nome do caminho')
     route = models.ForeignKey(Route, on_delete=models.CASCADE)
-    concentration = models.CharField(max_length=100, default='00:00', verbose_name='Horário de concentração')
-    time = models.CharField(max_length=30, verbose_name='Horário de Sáida')
+    concentration = models.CharField(max_length=100, default='----', verbose_name='Horário de concentração')
+    time = models.CharField(max_length=100, verbose_name='Horário de Sáida')
     departure_location = models.CharField(max_length=50, verbose_name='Local de Partida')
     active = models.BooleanField(default=False, verbose_name='Estado da Rota')
 

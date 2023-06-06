b6113523455fe9e164d42f8b22305da635cb1bd9
Seu Nome <3augustto3@gmail.com>
2023-06-06 14:56:55 -0300

    datepicker adjust

--- templates/static/js/helpers/urls.js
+++ templates/static/js/helpers/urls.js
@@ -1,5 +1,5 @@
 export const Urls = {
-    cities: 'http://200.132.47.33/api/cities/',
-    home_cities: 'http://200.132.47.33/api/home_cities/',
-    events: 'http://200.132.47.33/api/events/',
+    cities: 'http://127.0.0.1:8000/api/cities/',
+    home_cities: 'http://127.0.0.1:8000/api/home_cities/',
+    events: 'http://127.0.0.1:8000/api/events/',
 }

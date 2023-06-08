f3e000252b551f44d75d71aa0ef853cfc5f17942
Marcelo Augusto <3augustto3@gmail.com>
2023-06-07 23:01:41 -0300

    Login and register system

---
+++ users/models/user.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class User:
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.email

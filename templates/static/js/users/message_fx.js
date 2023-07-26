f4106dda72e4b3823d30980c42d786b7b542cc93
Marcelo Augusto <3augustto3@gmail.com>
2023-07-26 15:56:17 -0300

    Code improved

--- templates/static/js/users/message_fx.js
+++ templates/static/js/users/message_fx.js
@@ -1,19 +1,9 @@
 const messageCard = document.querySelector('.message-container');
 
-function hideMessageContainer() {
+window.onload = () => {
     if (messageCard) {
         setTimeout(() => {
-        messageCard.classList.add('hide');
+            messageCard.classList.add('hide');
         }, 5000);
     }
-}
-
-window.onload = () => {
-    hideMessageContainer();
-    const fadeOutDuration = 1000;
-    setTimeout(() => {
-        if (messageCard) {
-            messageCard.remove();
-        }
-    }, 5000 + fadeOutDuration);
 }

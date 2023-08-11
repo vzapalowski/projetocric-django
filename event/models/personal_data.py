e1cee0cdfa91a2833be9c1537f115a2267b9f139
Marcelo Augusto <3augustto3@gmail.com>
2023-08-11 14:32:35 -0300

    .

--- event/models/personal_data.py
+++ event/models/personal_data.py
@@ -5,7 +5,6 @@ from event.models.enrollment import Bond
 
 class PersonalData(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    full_name = models.CharField(max_length=100, verbose_name='Nome Completo')
     social_network = models.CharField(max_length=30, verbose_name='Instagram', null=True, blank=True)
     date_of_birth = models.DateField(verbose_name='Data de Nascimento')
     rg = models.CharField(max_length=10, verbose_name='RG')

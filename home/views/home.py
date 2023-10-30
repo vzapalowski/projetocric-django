9d97f1e6389b75de56a2d331c9b312675ebe5e98
Marcelo Augustto <3augustto3@gmail.com>
2023-10-30 13:59:41 -0300

    Participants count fixed

--- home/views/home.py
+++ home/views/home.py
@@ -4,7 +4,7 @@ from home.models import CityManager
 from cities.models import City
 from cities.models import Category
 from event.models import Event
-from event.models import Enrollment
+from event.models import Enrollment, EnrollmentType2
 
 
 class PostHome(ListView):
@@ -24,6 +24,9 @@ class PostHome(ListView):
             event.number_of_routes = event.routes_data.count()
             event.participants = Enrollment.objects.filter(event=event).count()
 
+            if not event.participants:
+                event.participants = EnrollmentType2.objects.filter(event=event).count()
+                
         context['events'] = events
     
         return context

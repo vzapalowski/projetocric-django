f6021e145a9c2ee9938678308f7f882d69559b47
Name <3augustto3@gmail.com>
2023-11-08 15:52:55 -0300

    Delete enrollment added

--- event/urls.py
+++ event/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('<int:pk>/', views.EventView.as_view(), name='event'),
     path('<int:event_id>/enrollment/', views.enrollment, name='enrollment'),
     path('<int:event_id>/enrollment2/', views.enrollment2, name='enrollment2'),
-    path('<int:event_id>/download/', views.download_pdf, name='download_pdf')
+    path('<int:event_id>/download/', views.download_pdf, name='download_pdf'),
+    path('<int:enrollment_id>/delete_enrollment/', views.delete_enrollment, name='delete_enrollment')
 ]

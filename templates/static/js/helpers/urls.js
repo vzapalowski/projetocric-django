d82a376f650d434902fe53d28347990cb312c38a
JeremiasPiontkoski <jeremiaspion@gmail.com>
2024-04-16 11:26:37 -0300

    Fixed bug send_form error

--- templates/static/js/helpers/urls.js
+++ templates/static/js/helpers/urls.js
@@ -1,11 +1,11 @@
 export const Urls = {
-    // cities: 'http://rota-cric.charqueadas.ifsul.edu.br/api/cities/',
-    // home_cities: 'http://rota-cric.charqueadas.ifsul.edu.br/api/home_cities/',
-    // events: 'http://rota-cric.charqueadas.ifsul.edu.br/api/events/',
-    // event_list: 'http://rota-cric.charqueadas.ifsul.edu.br/api/event_list/'
+    cities: 'http://rota-cric.charqueadas.ifsul.edu.br/api/cities/',
+    home_cities: 'http://rota-cric.charqueadas.ifsul.edu.br/api/home_cities/',
+    events: 'http://rota-cric.charqueadas.ifsul.edu.br/api/events/',
+    event_list: 'http://rota-cric.charqueadas.ifsul.edu.br/api/event_list/'
     
-    cities: 'http://127.0.0.1:8000/api/cities/',
-    home_cities: 'http://127.0.0.1:8000/api/home_cities/',
-    events: 'http://127.0.0.1:8000/api/events/',
-    event_list: 'http://127.0.0.1:8000/api/event_list/'
+    // cities: 'http://127.0.0.1:8000/api/cities/',
+    // home_cities: 'http://127.0.0.1:8000/api/home_cities/',
+    // events: 'http://127.0.0.1:8000/api/events/',
+    // event_list: 'http://127.0.0.1:8000/api/event_list/'
 }

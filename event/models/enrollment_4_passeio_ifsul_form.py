1e8ed518abbc96aed4224155bdd205953606100b
AntonioMartinss <antoniomartinssouza2@outlook.com>
2025-03-13 18:27:55 -0300

    ifsul fourth pedal form

---
+++ event/models/enrollment_4_passeio_ifsul_form.py
@@ -0,0 +1,21 @@
+from django import forms
+from event.models import Enrollment4PasseioCiclistico, Bond
+from event.models.route_path import RoutePath
+from projetocric.utilities import validate_field
+
+class enrollment4PasseioIfsulForm(forms.ModelForm):
+    class Meta:
+        model = Enrollment4PasseioCiclistico
+        fields = ('full_name', 'date_of_birth', 'bond_choice', 'how_knew', 'rg', 'route', 'event', 'user')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['route_path'].queryset = RoutePath.objects.filter(active=True)
+        self.fields['bond_choice'].queryset = Bond.objects.all()
+
+    class Meta:
+        model = Enrollment4PasseioCiclistico
+        fields = '__all__'
+
+    def clean_rg(self):
+        return validate_field(self, Enrollment4PasseioCiclistico, 'rg', 'Este RG já foi cadastrado!')

8467522b7cab79031a91b1cf15a374295510f99e
Vanius Zapalowski <vanius.zapalowski@gmail.com>
2025-03-22 14:23:06 +0000

    fix: fixing bug in new event

--- event/urls.py
+++ event/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('<int:event_id>/enrollment/', views.enrollment, name='enrollment'),
     path('<int:event_id>/enrollment2/', views.enrollment2, name='enrollment2'),
     path('<int:event_id>/enrollment3/', views.enrollment3, name='enrollment3'),
+    path('<int:event_id>/enrollment4/', views.enrollment4, name='enrollment4'),
     path('<int:event_id>/download/', views.download_pdf, name='download_pdf'),
     path('<int:enrollment_id>/delete_enrollment/', views.delete_enrollment, name='delete_enrollment'),
     path('<int:enrollment_id>/delete_enrollment_type2/', views.delete_enrollment_type2, name='delete_enrollment_type2'),

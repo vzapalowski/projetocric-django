0403db47cab3762ccfcd837dd44dc58f1d4e54fe
lucasnvs <lucasli.nev@hotmail.com>
2025-09-19 10:58:12 -0300

    refactor: restructure front-end templates and styling

--- templates/static/js/users/message_fx.js
+++
@@ -1,9 +0,0 @@
-const messageCard = document.querySelector('.message-container');
-
-window.onload = () => {
-    if (messageCard) {
-        setTimeout(() => {
-            messageCard.classList.add('hide');
-        }, 5000);
-    }
-}

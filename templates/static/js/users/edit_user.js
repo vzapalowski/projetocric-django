b2f203a14304a062f77a2948f41b83a226979239
lucasnvs <lucasli.nev@hotmail.com>
2025-09-22 11:54:55 -0300

    feat: date input validation

--- templates/static/js/users/edit_user.js
+++ templates/static/js/users/edit_user.js
@@ -1,6 +1,7 @@
-
+// -------------------------
+// Data de Nascimento
+// -------------------------
 const dateInput = document.getElementById('datepicker');
-const rgInput = document.getElementById('id_rg');
 
 dateInput.addEventListener('input', () => {
     let val = dateInput.value.replace(/\D/g, '');
@@ -12,9 +13,53 @@ dateInput.addEventListener('input', () => {
     dateInput.value = val;
 });
 
+dateInput.addEventListener('blur', () => {
+    const value = dateInput.value;
+    const regex = /^(\d{2})\/(\d{2})\/(\d{4})$/;
+
+    if(!regex.test(value)) {
+        dateInput.setCustomValidity("Data inválida. Use o formato dd/mm/aaaa.");
+        return;
+    }
+
+    let [, day, month, year] = value.match(regex).map(Number);
+
+    if(year < new Date().getFullYear() - 200) {
+        dateInput.setCustomValidity("Ano muito antigo.");
+        return;
+    }
+
+    if(month < 1 || month > 12) {
+        dateInput.setCustomValidity("Mês inválido.");
+        return;
+    }
+
+    if(day < 1 || day > 31) {
+        dateInput.setCustomValidity("Dia inválido.");
+        return;
+    }
+
+    const date = new Date(year, month - 1, day);
+    if(date.getDate() !== day || date.getMonth() !== month - 1 || date.getFullYear() !== year) {
+        dateInput.setCustomValidity("Data inválida. Verifique o dia, mês e ano.");
+        return;
+    }
+
+    dateInput.setCustomValidity("");
+});
+
+dateInput.addEventListener('input', () => {
+    dateInput.setCustomValidity("");
+});
+
+// -------------------------
+// RG
+// -------------------------
+const rgInput = document.getElementById('id_rg');
 
 rgInput.addEventListener('input', () => {
     rgInput.value = rgInput.value.replace(/\D/g,'').slice(0,10);
+    rgInput.setCustomValidity("");
 });
 
 rgInput.addEventListener('blur', () => {
@@ -23,9 +68,5 @@ rgInput.addEventListener('blur', () => {
         rgInput.setCustomValidity("RG inválido: deve ter 10 números.");
     } else {
         rgInput.setCustomValidity("");
-    }}
-);
-
-rgInput.addEventListener('input', () => {
-    rgInput.setCustomValidity("");
+    }
 });

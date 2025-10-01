6fa9b544e69e017de2a3e36bca8fbe7683cdf686
lucasnvs <lucasli.nev@hotmail.com>
2025-10-01 20:42:18 -0300

    fix: add app in sys path

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@
 import os
 import sys
 
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'app'))
 
 def main():
     """Run administrative tasks."""
@@ -16,7 +17,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()

370b1ce0dd5960667b33c6f251abb2990a29e715
pietrodonicht8@gmail.com <pietrodonicht8@gmail.com>
2025-11-07 01:07:31 -0300

    refactor(devs): reworked page logic to fetch collaborators from the database

---
+++ collaborators/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
+
+
+# faz uma table dos colabs
+class Collaborators(models.Model):
+    picture = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+
+class URL(models.Model):
+    collaborator = models.ForeignKey(Collaborators, on_delete=models.CASCADE, related_name='urls')
+    url = models.URLField(max_length=600, blank=True)
+
+
+#popular o banco com os devs pelo seed
+@receiver(post_migrate)
+def seed_data(sender, **kwargs):
+    if sender.label == 'collaborators':
+    
+        if Collaborators.objects.exists():
+            return
+        from .seed import run
+        run()
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-
 app_name = 'users'
 urlpatterns = [
     path('login/', views.login, name='login'),

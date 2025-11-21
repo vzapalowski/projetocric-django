47213e7a0ef03a038af2d69c8fc750acceb51a34
Mateus Peres <mateuspereslopesl@outlook.com>
2025-11-21 19:12:00 -0300

    feat: setting page not found screen + error screen

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def page_not_found(request, exception) : 
+  return render(request, 'not_found.html', status=404)
+
+def server_error(request):
+  return render(request, 'error.html', status=500)

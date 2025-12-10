5646951e238ff5d1c13d4d2009428809edffde24
lucasnvs <lucasli.nev@hotmail.com>
2025-12-10 20:57:54 -0300

    fix: debug

--- event/credentials.py
+++ event/credentials.py
@@ -1,4 +1,5 @@
 from decouple import config
 
-EMAIL = config('EMAIL_HOST_USER')
-PASSWORD = config('EMAIL_HOST_PASSWORD')
+# Default to empty values so tests and non-email flows don't crash on import.
+EMAIL = config('EMAIL_HOST_USER', default='')
+PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
